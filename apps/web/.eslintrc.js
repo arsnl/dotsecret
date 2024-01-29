c1047f6e122b124a520da228ab08eae1a4745b63
Mark Arseneault <mark@arsnl.dev>
2024-01-28 19:12:52 -0500

    chore: change web app for astro

--- apps/web/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["peppy", "peppy/next"],
-  parserOptions: {
-    project: true,
-  },
-};
--- packages/ui/src/code.tsx
+++
@@ -1,7 +0,0 @@
-export const Code = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}): JSX.Element => <code className={className}>{children}</code>;
