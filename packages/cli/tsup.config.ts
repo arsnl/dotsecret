67465eee9afe981324e4ccaf12c9834928c4432b
Mark Arseneault <mark@arsnl.dev>
2024-01-30 16:30:08 -0500

    chore: change cli build to use esm

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -4,19 +4,9 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/cli.ts"],
   outDir: "dist",
-  dts: true,
-  splitting: false,
-  sourcemap: true,
-  // We need to include theses packages in the bundle since they are ESM only and need to be converted to CJS.
-  noExternal: [
-    "boxen",
-    "chalk",
-    "color-json",
-    "execa",
-    "find-up",
-    "globby",
-    "multimatch",
-    "ora",
-  ],
+  dts: false,
+  splitting: true,
+  sourcemap: false,
+  format: ["esm"],
   clean: true,
 });
