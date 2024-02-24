66b5a7b9e9803b8c1d3303c308c7edf9d0c53e53
Mark Arseneault <mark@arsnl.dev>
2024-02-23 19:37:00 -0500

    chore: fix enoent

--- scripts/release.ts
+++ scripts/release.ts
@@ -6,21 +6,21 @@ import nodeFs from "node:fs";
 import nodePath from "node:path";
 
 const cwd = nodePath.join(__dirname, "..");
-const changesetStatusPath = nodePath.join(cwd, "changeset-status.json");
-const changesetPrePath = nodePath.join(cwd, ".changeset/pre.json");
-const isNext = process.argv.includes("--next");
+const next = process.argv.includes("--next");
 const verbose = process.argv.includes("--verbose");
 
 const hasChangesets = async () => {
-  const cmd = `npx changeset status --since=${isNext ? "next" : "main"}  --output=${changesetStatusPath}`;
+  const changesetFile = "./changeset-status.json";
 
   try {
-    verbose && console.log(`Running: ${cmd}`);
-
-    await $({ stdio: "ignore", cwd })`${cmd}`;
+    await $({
+      stdio: "ignore",
+      cwd,
+      verbose,
+    })`npx changeset status --since=${next ? "next" : "main"} --output=${changesetFile}`;
 
     const changesetStatus = JSON.parse(
-      nodeFs.readFileSync(changesetStatusPath, "utf-8"),
+      nodeFs.readFileSync(changesetFile, "utf-8"),
     ) as { changesets: any[]; releases: any[] };
 
     verbose && console.log("Changeset status:", changesetStatus);
@@ -37,7 +37,7 @@ const hasChangesets = async () => {
 const isPreMode = () => {
   try {
     const changesetConfig = JSON.parse(
-      nodeFs.readFileSync(changesetPrePath, "utf-8"),
+      nodeFs.readFileSync(".changeset/pre.json", "utf-8"),
     ) as any;
 
     return changesetConfig?.mode === "pre";
@@ -48,14 +48,17 @@ const isPreMode = () => {
 
 (async () => {
   if (verbose) {
-    console.log("Current directory:", cwd);
-    console.log("Changeset status path:", changesetStatusPath);
-    console.log("Is next:", isNext);
-    console.log("Changeset pre path:", changesetPrePath);
-    console.log("Is pre mode:", isPreMode());
+    console.log("cwd:", cwd);
+    console.log("next:", next);
+    console.log("pre:", isPreMode());
+    console.log("");
   }
 
-  if (isNext && !isPreMode()) {
+  await hasChangesets();
+
+  process.exit(0);
+
+  if (next && !isPreMode()) {
     await $({ cwd })`npx changeset pre enter next`;
 
     if (!isPreMode()) {
@@ -64,7 +67,7 @@ const isPreMode = () => {
     }
   }
 
-  if (!isNext && isPreMode()) {
+  if (!next && isPreMode()) {
     await $({ cwd })`npx changeset pre exit`;
 
     if (isPreMode()) {
